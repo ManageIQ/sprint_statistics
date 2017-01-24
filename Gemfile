ca644062a5bd56d8455af1dae569d5a12b4a6051
Brandon Dunne <bdunne@redhat.com>
2017-01-24 13:45:28 -0500

    Restructure closed_issues to a class with a few basic methods.

--- Gemfile
+++ Gemfile
@@ -1,1 +1,4 @@
-gem 'octokit', "~>4.3.0", :require => false
+source 'https://rubygems.org'
+
+gem 'activesupport', "~> 5.0",   :require => false
+gem 'octokit',       "~> 4.3.0", :require => false
--- closed_issues.rb
+++ closed_issues.rb
@@ -1,18 +1,42 @@
 MILESTONE = 36
 ACCESS_TOKEN = "your github access token"
 
-require 'octokit'
-o = Octokit::Client.new(:access_token => ACCESS_TOKEN)
+require 'active_support'
+require 'active_support/core_ext'
 
-results = []
-page = 1
+class MiqSprintStatistics
+  def initialize(access_token, milestone = nil)
+    @access_token = access_token
+    @milestone    = milestone
+  end
 
-loop do
-  response = o.issues("ManageIQ/manageiq", :milestone => MILESTONE, :state => "closed", :page => page)
-  break if response == []
-  results += response
-  page += 1
+  def client
+    @client ||= begin
+      require 'octokit'
+      Octokit::Client.new(:access_token => @access_token)
+    end
+  end
+
+  def paginated_fetch(collection, *args)
+    options          = args.extract_options!
+    options[:page] ||= 1
+
+    results = []
+    loop do
+      response = client.send(collection, *args, options)
+      break if response == []
+      results += response
+      options[:page] += 1
+    end
+    results
+  end
+
+  def results
+    paginated_fetch(:issues, "ManageIQ/manageiq", :milestone => @milestone, :state => "closed")
+  end
 end
+
+results = MiqSprintStatistics.new(ACCESS_TOKEN, MILESTONE).results
 
 # Reject issues that are not Pull Requests
 results.reject { |i| !i.pull_request? }
