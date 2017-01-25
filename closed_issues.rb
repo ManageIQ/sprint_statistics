31fd7a6a624b8ede53233a6780eb5cadbc740001
mfeifer <mfeifer@redhat.com>
2017-01-25 12:20:21 -0500

    Add Milestone comment and csv file
    There is an offset of 6 between numerical ID of Sprint Milestone and its description. Adds a comment that you need to subtract 6.
    Creates csv output file without intervention
    
    Output to csv file
    
    Output goes to csv while retaining the on-screen progress notifications
    
    [skip ci]

--- closed_issues.rb
+++ closed_issues.rb
@@ -1,12 +1,14 @@
-MILESTONE = 36
+MILESTONE = 47
+# subtract 6 from actual Sprint milestone number for the ManageIQ/manageiq repo
 ACCESS_TOKEN = "your github access token"
 
 require_relative 'sprint_statistics'
 prs = SprintStatistics.new(ACCESS_TOKEN).pull_requests("ManageIQ/manageiq", :milestone => MILESTONE, :state => "closed")
 
-puts "Milestone Statistics for: #{prs.first.milestone.title}"
-puts "NUMBER,TITLE,AUTHOR,ASSIGNEE,LABELS"
-puts "--------------------------------------------------"
-prs.each do |i|
-  puts "#{i.number},#{i.title},#{i.user.login},#{i.assignee && i.assignee.login},#{i.labels.collect(&:name).join(" ")}"
+File.open('closed_issues_master_repo.csv', 'w') do |f|
+    f.puts "Milestone Statistics for: #{prs.first.milestone.title}"
+    f.puts "NUMBER,TITLE,AUTHOR,ASSIGNEE,LABELS"
+    prs.each do |i|
+    	f.puts "#{i.number},#{i.title},#{i.user.login},#{i.assignee && i.assignee.login},#{i.labels.collect(&:name).join(" ")}"
+    end
 end
