ad955d6242674f9414dea1ccc4ab8ad4706816e6
mfeifer <mfeifer@redhat.com>
2017-01-27 10:24:26 -0500

    Add changelog text to output
    Adds the string used in the changelog file to the output.
    use url instead of awkward concatenation
    match column labels to output
    
    [skip ci]

--- closed_issues.rb
+++ closed_issues.rb
@@ -7,8 +7,9 @@ prs = SprintStatistics.new(ACCESS_TOKEN).pull_requests("ManageIQ/manageiq", :mil
 
 File.open('closed_issues_master_repo.csv', 'w') do |f|
     f.puts "Milestone Statistics for: #{prs.first.milestone.title}"
-    f.puts "NUMBER,TITLE,AUTHOR,ASSIGNEE,LABELS"
+    f.puts "NUMBER,TITLE,AUTHOR,ASSIGNEE,LABELS,CLOSED AT,CHANGELOGTEXT"
     prs.each do |i|
-    	f.puts "#{i.number},#{i.title},#{i.user.login},#{i.assignee && i.assignee.login},#{i.labels.collect(&:name).join(" ")}"
+    	i.changelog = "#{i.title} #[#{i.number}](#{i.pull_request.html_url})"
+    	f.puts "#{i.number},#{i.title},#{i.user.login},#{i.assignee && i.assignee.login},#{i.labels.collect(&:name).join(" ")},#{i.closed_at},#{i.changelog}"
     end
 end
