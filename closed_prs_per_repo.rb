f8baabc174b62987e5ab4726e8e75a4154fd22f7
Brandon Dunne <bdunne@redhat.com>
2017-05-08 11:48:20 -0400

    Track closed and opened pull requests.
    Add :since option to limit the data coming back from the API
    Change :state to :all (open and closed)
    With the :since filter, we can also collect "ManageIQ/manageiq"

--- closed_prs_per_repo.rb
+++ closed_prs_per_repo.rb
@@ -27,13 +27,15 @@ end
 
 
 results = []
-(repos_to_track - ["ManageIQ/manageiq"]).sort.each do |repo| # manageiq is huge, please get its details from closed_issues.rb
-  puts "Collecting pull_requests closed for: #{repo}"
-  prs = stats.pull_requests(repo, :state => "closed").select { |pr| sprint_range.include?(pr.closed_at) }
-  results << "#{repo},#{prs.length}"
+repos_to_track.sort.each do |repo|
+  puts "Collecting pull_requests for: #{repo}"
+  prs = stats.pull_requests(repo, :state => :all, :since => sprint_range.first.iso8601)
+  closed = prs.select { |pr| sprint_range.include?(pr.closed_at) }
+  opened = prs.select { |pr| sprint_range.include?(pr.created_at) }
+  results << "#{repo},#{opened.length},#{closed.length}"
 end
 
-File.open('closed_prs_per_repo.csv', 'w') do |f|
-  f.puts "Pull Requests closed from: #{sprint_range.first} to: #{sprint_range.last}"
+File.open('prs_per_repo.csv', 'w') do |f|
+  f.puts "Pull Requests from: #{sprint_range.first} to: #{sprint_range.last}.  repo,#opened,#closed"
   results.each { |line| f.puts(line) }
 end
