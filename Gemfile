2dbb2d4a1786176fdb0d16c4e410653c7bd5d9ca
Brandon Dunne <bdunne@redhat.com>
2017-05-16 10:28:12 -0400

    Add selected label counts for closed PRs

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'activesupport', "~> 5.0",   :require => false
+gem 'more_core_extensions',      :require => false
 gem 'octokit',       "~> 4.3.0", :require => false
--- prs_per_repo.rb
+++ prs_per_repo.rb
@@ -25,18 +25,20 @@ def repos_to_track
   stats.project_names_from_org("ManageIQ").to_a + ["Ansible/ansible_tower_client_ruby"]
 end
 
-
+labels  = ["bug", "enhancement", "developer", "documentation", "performance", "refactoring", "technical debt", "test", ]
 results = []
 repos_to_track.sort.each do |repo|
   puts "Collecting pull_requests for: #{repo}"
   prs                = stats.pull_requests(repo, :state => :all, :since => sprint_range.first.iso8601)
   closed             = prs.select { |pr| sprint_range.include?(pr.closed_at) }
+  closed_labels_hash = closed.each_with_object([]) { |pr, arr| pr.labels.each { |label| arr << label.name } }.element_counts
   opened             = prs.select { |pr| sprint_range.include?(pr.created_at) }
   prs_remaining_open = stats.raw_pull_requests(repo, :state => :open).length
-  results << "#{repo},#{opened.length},#{closed.length},#{prs_remaining_open}"
+  labels_string      = closed_labels_hash.values_at(*labels).collect(&:to_i).join(",")
+  results << "#{repo},#{opened.length},#{closed.length},#{labels_string},#{prs_remaining_open}"
 end
 
 File.open('prs_per_repo.csv', 'w') do |f|
-  f.puts "Pull Requests from: #{sprint_range.first} to: #{sprint_range.last}.  repo,#opened,#closed,#remaining_open"
+  f.puts "Pull Requests from: #{sprint_range.first} to: #{sprint_range.last}.  repo,#opened,#closed,#{labels.collect { |l| "closed_#{l}" }.join(",")},#remaining_open"
   results.each { |line| f.puts(line) }
 end
