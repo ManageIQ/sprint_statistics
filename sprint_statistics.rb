e407431017506fc52be84cda1200b153b86285e7
Jason Frey <jfrey@redhat.com>
2019-11-27 14:41:02 -0500

    Add Faraday middleware for debugging API requests

--- sprint_statistics.rb
+++ sprint_statistics.rb
@@ -34,7 +34,26 @@ class SprintStatistics
   def client
     @client ||= begin
       require 'octokit'
-      Octokit.auto_paginate = true
+      Octokit.configure do |c|
+        c.auto_paginate = true
+        c.middleware = Faraday::RackBuilder.new do |builder|
+          if ENV['DEBUG']
+            builder.use(
+              Class.new(Faraday::Response::Middleware) do
+                def on_complete(env)
+                  api_calls_remaining = env.response_headers['x-ratelimit-remaining']
+                  STDOUT.puts "DEBUG: Executed #{env.method.to_s.upcase} #{env.url} ... api calls remaining #{api_calls_remaining}"
+                end
+              end
+            )
+          end
+
+          builder.use Octokit::Response::RaiseError
+          builder.use Octokit::Response::FeedParser
+          builder.adapter Faraday.default_adapter
+        end
+      end
+
       Octokit::Client.new(:access_token => @access_token)
     end
   end
